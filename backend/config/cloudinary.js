f84b02e51acf4626795577eac5964b2d09e71fcd
user.name : "ygirish890@gmail.com <user.email.like : "ygirish890@gmail.com>
2026-08-20 12:08:24 +0530

    fix vercel

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,6 +1,8 @@
 import {v2 as cloudinary} from "cloudinary"
 
 const connectCloudinary = () => {
+    console.log("CLOUD NAME:", process.env.CLOUDINARY_NAME);
+    console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
